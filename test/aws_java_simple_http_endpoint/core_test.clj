ba629eeb08e3567809240782f1378b834058a84a
steffen <steffenengel@live.com>
2025-01-11 17:05:54 +0100

    Enhance test

--- test/aws_java_simple_http_endpoint/core_test.clj
+++ test/aws_java_simple_http_endpoint/core_test.clj
@@ -1,6 +1,10 @@
 (ns aws-java-simple-http-endpoint.core-test
-  (:require [clojure.test :refer [deftest testing is]]
-            [aws-clojure-simple-http-endpoint.core :refer [lambda-integration-response -handleRequest]]))
+  (:require
+   [aws-clojure-simple-http-endpoint.core :refer [-handleRequest
+                                                  lambda-integration-response]]
+   [clojure.data.json :as json]
+   [clojure.string :as str]
+   [clojure.test :refer [deftest is testing]]))
 
 (deftest lambda-integration-reponse-test
   (testing "Test lambda-integration-response"
@@ -9,9 +13,9 @@
       (is (= "Hello" (.getBody result)))
       (is (= 200 (.getStatusCode result))))))
 
-
 (deftest handlne-request-test
   (testing "Test handle-request"
     (let [result (-handleRequest nil nil nil)]
       (is (= {"Content-Type" "application/json", "Access-Control-Allow-Origin" "*"} (.getHeaders result)))
+      (is (str/starts-with? (-> (.getBody result) json/read-str (get "message")) "Hello" ))
       (is (= 200 (.getStatusCode result))))))
